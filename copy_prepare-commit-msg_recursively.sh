bc11d2d1da4a2b6bb37b7b6452412fcfcfb49e28
shinespark <shnsprk@gmail.com>
2021-11-15 15:56:00 +0900

    ファイルの存在チェックをやめた

--- copy_prepare-commit-msg_recursively.sh
+++ copy_prepare-commit-msg_recursively.sh
@@ -10,8 +10,5 @@ repos=`'ls' -p $dir | grep /`
 
 for repo in ${repos[@]}; do
   to_file_path=${dir}/${repo}.git/hooks/prepare-commit-msg
-  if [ -f $to_file_path ]; then
-    # cp -iv ./prepare-commit-msg $to_file_path
-    cp -v ./prepare-commit-msg $to_file_path
-  fi
+  cp -v ./prepare-commit-msg $to_file_path
 done
